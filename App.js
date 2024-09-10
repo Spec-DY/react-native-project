7527b7555524977c60e4d8aa93f361a9595f62a9
Spec-DY <ibonehp@gmail.com>
2024-09-10 15:05:17 -0700

    add input text display

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from "./components/Header.js";
+import { useState } from 'react';
 
 export default function App() {
-
+  const [text, setText] = useState('')
   return (
     <View style={styles.container}>
       <Header name = "Thunder" />
+        <TextInput
+          style = {{borderWidth: 3}}
+          autoCorrect={true}
+          placeholder='Enter here'
+          onChangeText={(changedText) => {
+            setText(changedText);
+            console.log("1");
+          }}
+          value={text}
+          keyboardType='default'/>
       <StatusBar style="auto" />
+      
+      <Text>
+        typed:{text}
+      </Text>
     </View>
 
   );

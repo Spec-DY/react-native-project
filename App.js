88c3770855e8af2cf3ea58cb1f45ce280d94bb91
Spec-DY <ibonehp@gmail.com>
2024-09-16 18:39:47 -0700

    Add Button and Modal

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Pressable, StyleSheet, Text, TextInput, View, Button } from 'react-native';
 import Header from "./components/Header.js";
 import { useState } from 'react';
 import Input from "./components/Input.js";
 
 export default function App() {
+  const [text, setText] = useState("");
+  const [modalVisibility, setModalVisibility] = useState(false);
+
+  function handleInputData(inputData) {
+    setText(inputData)
+    setModalVisibility(false);
+  }
+
+  function handleModalVisibility(){
+    setModalVisibility(!modalVisibility)
+  }
   return (
     <View style={styles.container}>
       <Header name = "Thunder" />
       <StatusBar style="auto" />
-      <Input autoFocus={true}/>
-      {/* <Text>typed:{text}</Text> */}
+      <Input autoFocus={true} onConfirm={handleInputData} modalVisibility={modalVisibility}/>
+      <Button title="Add a goal" onPress={handleModalVisibility} />
+      <Text>typed:{text}</Text>
     </View>
 
   );
--- components/Input.js
+++ components/Input.js
@@ -1,39 +1,59 @@
 import { useState } from "react";
-import { View, TextInput, Text, StyleSheet } from "react-native";
+import { View, TextInput, Text, StyleSheet, Button, Modal } from "react-native";
 
-const Input = ({autoFocus}) => {
+const Input = ({autoFocus, onConfirm, modalVisibility}) => {
     const [text, setText] = useState('')
     const [hasBlurred, setHasBlurred] = useState('')
     const handleBlur = ()=> {setHasBlurred(true)};
     const handleFocus = ()=> {setHasBlurred(false)};
+    function handleConfirm() {
+            onConfirm(text)
+    }
+
 
     return(
-        <View>
-            <TextInput
-                style = {{borderWidth: 3}}
-                autoCorrect={true}
-                placeholder='Enter here'
-                onChangeText={(changedText) => {
-                setText(changedText);
-                }}
-                value={text}
-                keyboardType='default'
-                autoFocus={autoFocus}
-                onBlur={handleBlur}
-                onFocus={handleFocus}
-                />
-            {text.length > 0 && !hasBlurred && (
-                <Text>{`Character count: ${text.length}`}</Text>
-            )}
+        <Modal
+            animationType="slide"
+            transparent={false}
+            visible={modalVisibility}>
+            <View style={styles.container}>
+                <TextInput
+                    style = {{borderWidth: 2}}
+                    autoCorrect={true}
+                    placeholder='Enter here'
+                    onChangeText={(changedText) => {
+                    setText(changedText);
+                    }}
+                    value={text}
+                    keyboardType='default'
+                    autoFocus={autoFocus}
+                    onBlur={handleBlur}
+                    onFocus={handleFocus}
+                    />
+                {text.length > 0 && !hasBlurred && (
+                    <Text>{`Character count: ${text.length}`}</Text>
+                )}
 
-            {hasBlurred && (
-                <Text>
-                    {text.length >= 3 ? "Thank you" : "Please type more than 3 characters"}
-                </Text>
-            )}
-    </View>
+                {hasBlurred && (
+                    <Text>
+                        {text.length >= 3 ? "Thank you" : "Please type more than 3 characters"}
+                    </Text>
+                )}
+
+                <Button title="Confirm" onPress={handleConfirm}/>
+            </View>
+        </Modal>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+});
 
 export default Input;
 

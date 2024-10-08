d22e4b53d843b206ec8fde5b891d32f4d0845822
Spec-DY <ibonehp@gmail.com>
2024-10-08 13:47:38 -0700

    add pressable and styling

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,7 +1,7 @@
-import {View, Text, StyleSheet, Button} from "react-native";
+import {View, Text, StyleSheet, Button, Pressable} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-const GoalItem=({goal, onDelete, onGoalPress}) => {
+const GoalItem=({goal, onDelete}) => {
     console.log(goal)
     const navigation = useNavigation();
 
@@ -10,18 +10,29 @@ const GoalItem=({goal, onDelete, onGoalPress}) => {
     }
   
     return (
-        <View style={styles.container}>
-            <Text style={styles.goalText}>{goal.text}</Text>
-            <Button
-                title="X"
-                color="red" 
-                onPress={() => onDelete(goal.id)}
-            />
-            <View style={{margin:5}} />
-            <Button title="i" onPress={handleNavigate} />
-        </View>
-    )
-}
+      <Pressable 
+      style={({ pressed }) => [
+        styles.container,
+        pressed && styles.pressedContainer
+      ]}
+      onPress={handleNavigate}
+      android_ripple={{ color: "lightgrey" }}
+      >
+      <Text style={styles.goalText}>{goal.text}</Text>
+      <View style={{ margin: 5 }} />
+      <Pressable
+        style={({ pressed }) => [
+          styles.deleteButton,
+          pressed && styles.pressedDeleteButton
+        ]}
+        onPress={() => onDelete(goal.id)}
+        android_ripple={{ color: "red" }}
+      >
+        <Text style={styles.deleteText}>X</Text>
+      </Pressable>
+    </Pressable>
+  );
+};
 
 const styles = StyleSheet.create({
     container: {
@@ -34,8 +45,22 @@ const styles = StyleSheet.create({
     },
     goalText: {
       fontSize: 20,
-      color: 'grey',
+      color: 'grey'
     },
+    deleteText: {
+      fontSize: 16,
+      color: 'red',
+    },
+    pressedContainer: {
+      backgroundColor: "lightgrey",
+    },
+    pressedDeleteButton: {
+      backgroundColor: "red",
+    },
+    deleteButton: {
+      padding: 5,
+      borderRadius: 5,
+    }
   });
   
   export default GoalItem;
